92497dc4be37a136c9a76a06cfd665f88a3e4dc8
carnivalgoblin <rico.prodan@gmail.com>
2022-05-03 12:38:08 +0200

    completed tasks

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+function newItem() {
+    /* ADD ITEM */
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must enter something!');
+    } else {
+        $('#list').append(li);
+    }
+
+
+    /* STRIKE OUT */
+    function strikeOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', function strikeOut() {
+        li.toggleClass('strike');
+    });
+
+    /* DELETE */
+    let deleteButton = $('<deleteButton></deleteButton>');
+
+    deleteButton.append(document.createTextNode('X'));
+    li.append(deleteButton);
+    deleteButton.on('click', deleteItem);
+
+    function deleteItem() {
+        li.addClass('delete');
+    }
+
+    /* REORDER */
+    $('#list').sortable();
+
+}
